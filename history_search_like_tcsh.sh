745998e9f2a6fd22bbf8c22fdc5fcc9dfe720e9d
Masamitsu MURASE <masamitsu.murase@gmail.com>
2020-12-31 01:50:35 +0900

    Rewrite shell script using array.

--- history_search_like_tcsh.sh
+++ history_search_like_tcsh.sh
@@ -1,12 +1,12 @@
 
-__bhslt_prefix=""
-__bhslt_current_match=""
+__bhslt_current_match_index=-1
+__bhslt_history_array=()
 __bhslt_readline_line=""
 __bhslt_readline_point=-1
 
 __bhslt_clear_state() {
-    __bhslt_prefix=""
-    __bhslt_current_match=""
+    __bhslt_current_match_index=-1
+    __bhslt_history_array=()
     __bhslt_readline_line=""
     __bhslt_readline_point=-1
 }
@@ -20,75 +20,49 @@ __bhslt_check_state() {
 __bhslt_search_backward() {
     __bhslt_check_state
 
-    if [ "$__bhslt_current_match" = "" ]; then
-        __bhslt_prefix="${READLINE_LINE:0:$READLINE_POINT}"
-    fi
+    if [ $__bhslt_current_match_index -eq -1 ]; then
+        local PREFIX="${READLINE_LINE:0:$READLINE_POINT}"
 
-    { coproc FC_FD { fc -lnr -$HISTSIZE ; } } 2>/dev/null
-    local PREFIX_LEN=${#__bhslt_prefix}
-    local FOUND=0
-    local MATCHED_LINE=""
-    declare -A HISTORY_HASH
-    if [ "$__bhslt_current_match" = "" ]; then
-        FOUND=1
-    fi
-    while read -u ${FC_FD[0]} LINE; do
-        if [ "$LINE" != "" ] && [ -z "${HISTORY_HASH[$LINE]}" ] && [ "${LINE:0:$PREFIX_LEN}" = "$__bhslt_prefix" ]; then
-            HISTORY_HASH[$LINE]="1"
-
-            if [ $FOUND -eq 0 ]; then
-                if [ "$__bhslt_current_match" = "$LINE" ]; then
-                    FOUND=1
-                fi
-            else
-                MATCHED_LINE="$LINE"
-                read -u ${FC_FD[0]} -d ''
-                break
+        { coproc FC_FD { fc -lnr -$HISTSIZE ; } ; } 2>/dev/null
+        local PREFIX_LEN=${#PREFIX}
+        declare -A HISTORY_HASH
+        while read -u ${FC_FD[0]} LINE; do
+            if [ "$LINE" != "" ] && [ -z "${HISTORY_HASH[$LINE]}" ] && [ "${LINE:0:$PREFIX_LEN}" = "$PREFIX" ]; then
+                HISTORY_HASH[$LINE]="1"
+                __bhslt_history_array+=( "$LINE" )
             fi
-        fi
-    done
-    wait $FC_FD_PID 2>/dev/null
+        done
+        wait $FC_FD_PID 2>/dev/null
 
-    if [ "$MATCHED_LINE" != "" ]; then
-        READLINE_LINE="${MATCHED_LINE}"
-        READLINE_POINT="${#READLINE_LINE}"
-        __bhslt_current_match="${MATCHED_LINE}"
-        __bhslt_readline_line=$READLINE_LINE
-        __bhslt_readline_point=$READLINE_POINT
+        if [ ${#__bhslt_history_array[@]} -gt 0 ]; then
+            __bhslt_current_match_index=0
+            READLINE_LINE="${__bhslt_history_array[$__bhslt_current_match_index]}"
+            READLINE_POINT="${#READLINE_LINE}"
+            __bhslt_readline_line=$READLINE_LINE
+            __bhslt_readline_point=$READLINE_POINT
+        fi
+    else
+        if [ $(($__bhslt_current_match_index+1)) -lt ${#__bhslt_history_array[@]} ]; then
+            __bhslt_current_match_index=$(($__bhslt_current_match_index+1))
+            READLINE_LINE="${__bhslt_history_array[$__bhslt_current_match_index]}"
+            READLINE_POINT="${#READLINE_LINE}"
+            __bhslt_readline_line=$READLINE_LINE
+            __bhslt_readline_point=$READLINE_POINT
+        fi
     fi
 }
 
 __bhslt_search_forward() {
     __bhslt_check_state
 
-    if [ "$__bhslt_current_match" = "" ]; then
-        return
-    fi
-
-    { coproc FC_FD { fc -lnr -$HISTSIZE ; } } 2>/dev/null
-    local PREFIX_LEN=${#__bhslt_prefix}
-    declare -A HISTORY_HASH
-    local LAST_MATCHED_LINE=""
-    while read -u ${FC_FD[0]} LINE; do
-        if [ "$LINE" != "" ] && [ -z "${HISTORY_HASH[$LINE]}" ] && [ "${LINE:0:$PREFIX_LEN}" = "$__bhslt_prefix" ]; then
-            HISTORY_HASH[$LINE]="1"
-
-            if [ "$__bhslt_current_match" = "$LINE" ]; then
-                read -u ${FC_FD[0]} -d ''
-                break
-            else
-                LAST_MATCHED_LINE="$LINE"
-            fi
+    if [ $__bhslt_current_match_index -ne -1 ]; then
+        if [ $__bhslt_current_match_index -gt 0 ]; then
+            __bhslt_current_match_index=$(($__bhslt_current_match_index-1))
+            READLINE_LINE="${__bhslt_history_array[$__bhslt_current_match_index]}"
+            READLINE_POINT="${#READLINE_LINE}"
+            __bhslt_readline_line=$READLINE_LINE
+            __bhslt_readline_point=$READLINE_POINT
         fi
-    done
-    wait $FC_FD_PID 2>/dev/null
-
-    if [ "$LAST_MATCHED_LINE" != "" ]; then
-        READLINE_LINE="${LAST_MATCHED_LINE}"
-        READLINE_POINT="${#READLINE_LINE}"
-        __bhslt_current_match="${LAST_MATCHED_LINE}"
-        __bhslt_readline_line=$READLINE_LINE
-        __bhslt_readline_point=$READLINE_POINT
     fi
 }
 
