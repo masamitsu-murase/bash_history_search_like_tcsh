ea0ab6cc19bc57d4808eab62de2a0bcdb734415d
Masamitsu MURASE <masamitsu.murase@gmail.com>
2020-12-31 00:57:15 +0900

    Use fc to get history.

--- history_search_like_tcsh.sh
+++ history_search_like_tcsh.sh
@@ -1,19 +1,14 @@
 
-__bhslt_python_path="python"
-__bhslt_python_src_path="/path/to/search_history.py"
-
-__bhslt_coproc_fd="None"
+__bhslt_prefix=""
+__bhslt_current_match=""
 __bhslt_readline_line=""
 __bhslt_readline_point=-1
 
 __bhslt_clear_state() {
-    if [ "$__bhslt_coproc_fd" != "None" ] ; then
-        __bhslt_readline_line=""
-        __bhslt_readline_point=-1
-        echo '<quit>' >&${__bhslt_coproc_fd[1]}
-        wait $__bhslt_coproc_fd_PID 2>/dev/null
-        __bhslt_coproc_fd="None"
-    fi
+    __bhslt_prefix=""
+    __bhslt_current_match=""
+    __bhslt_readline_line=""
+    __bhslt_readline_point=-1
 }
 
 __bhslt_check_state() {
@@ -25,32 +20,73 @@ __bhslt_check_state() {
 __bhslt_search_backward() {
     __bhslt_check_state
 
-    if [ "$__bhslt_coproc_fd" = "None" ] ; then
-        export BHSLT_PREFIX="${READLINE_LINE:0:$READLINE_POINT}"
-        { coproc __bhslt_coproc_fd { $__bhslt_python_path $__bhslt_python_src_path ; } } 2>/dev/null
-        unset BHSLT_PREFIX
-        HISTTIMEFORMAT=' ' history >&${__bhslt_coproc_fd[1]}
-        echo '<history end>' >&${__bhslt_coproc_fd[1]}
-    else
-        echo '<previous match>' >&${__bhslt_coproc_fd[1]}
+    if [ "$__bhslt_current_match" = "" ]; then
+        __bhslt_prefix="${READLINE_LINE:0:$READLINE_POINT}"
     fi
-    read -u ${__bhslt_coproc_fd[0]} match_command
 
-    READLINE_LINE="${match_command:0:-1}"
-    READLINE_POINT="${#READLINE_LINE}"
-    __bhslt_readline_line=$READLINE_LINE
-    __bhslt_readline_point=$READLINE_POINT
+    { coproc FC_FD { fc -lnr -$HISTSIZE ; } } 2>/dev/null
+    local PREFIX_LEN=${#__bhslt_prefix}
+    local FOUND=0
+    local MATCHED_LINE=""
+    declare -A HISTORY_HASH
+    if [ "$__bhslt_current_match" = "" ]; then
+        FOUND=1
+    fi
+    while read -u ${FC_FD[0]} LINE; do
+        if [ "$LINE" != "" ] && [ -z "${HISTORY_HASH[$LINE]}" ] && [ "${LINE:0:$PREFIX_LEN}" = "$__bhslt_prefix" ]; then
+            HISTORY_HASH[$LINE]="1"
+
+            if [ $FOUND -eq 0 ]; then
+                if [ "$__bhslt_current_match" = "$LINE" ]; then
+                    FOUND=1
+                fi
+            else
+                MATCHED_LINE="$LINE"
+                read -u ${FC_FD[0]} -d ''
+                break
+            fi
+        fi
+    done
+    wait $FC_FD_PID 2>/dev/null
+
+    if [ "$MATCHED_LINE" != "" ]; then
+        READLINE_LINE="${MATCHED_LINE}"
+        READLINE_POINT="${#READLINE_LINE}"
+        __bhslt_current_match="${MATCHED_LINE}"
+        __bhslt_readline_line=$READLINE_LINE
+        __bhslt_readline_point=$READLINE_POINT
+    fi
 }
 
 __bhslt_search_forward() {
     __bhslt_check_state
 
-    if [ "$__bhslt_coproc_fd" != "None" ] ; then
-        echo '<next match>' >&${__bhslt_coproc_fd[1]}
-        read -u ${__bhslt_coproc_fd[0]} match_command
+    if [ "$__bhslt_current_match" = "" ]; then
+        return
+    fi
 
-        READLINE_LINE="${match_command:0:-1}"
+    { coproc FC_FD { fc -lnr -$HISTSIZE ; } } 2>/dev/null
+    local PREFIX_LEN=${#__bhslt_prefix}
+    declare -A HISTORY_HASH
+    local LAST_MATCHED_LINE=""
+    while read -u ${FC_FD[0]} LINE; do
+        if [ "$LINE" != "" ] && [ -z "${HISTORY_HASH[$LINE]}" ] && [ "${LINE:0:$PREFIX_LEN}" = "$__bhslt_prefix" ]; then
+            HISTORY_HASH[$LINE]="1"
+
+            if [ "$__bhslt_current_match" = "$LINE" ]; then
+                read -u ${FC_FD[0]} -d ''
+                break
+            else
+                LAST_MATCHED_LINE="$LINE"
+            fi
+        fi
+    done
+    wait $FC_FD_PID 2>/dev/null
+
+    if [ "$LAST_MATCHED_LINE" != "" ]; then
+        READLINE_LINE="${LAST_MATCHED_LINE}"
         READLINE_POINT="${#READLINE_LINE}"
+        __bhslt_current_match="${LAST_MATCHED_LINE}"
         __bhslt_readline_line=$READLINE_LINE
         __bhslt_readline_point=$READLINE_POINT
     fi
