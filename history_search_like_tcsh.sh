84fd9e7e0d0d784145447d96e942c33ba155aa1e
Masamitsu MURASE <masamitsu.murase@gmail.com>
2021-01-03 00:12:44 +0900

    Fix handling of '\'.
    - "-r" option is added to "read" to handle '\' appropriately.
    - POSIXLY_CORRECT is also set to simplify 'fc' output.

--- history_search_like_tcsh.sh
+++ history_search_like_tcsh.sh
@@ -24,11 +24,14 @@ __bhslt_search_backward() {
     if [[ $__bhslt_current_match_index -eq -1 ]]; then
         local PREFIX="${READLINE_LINE:0:$READLINE_POINT}"
 
-        { coproc FC_FD { fc -lnr -${HISTSIZE:-1000} ; } ; } 2>/dev/null
+        { coproc FC_FD { POSIXLY_CORRECT="1" fc -lnr -${HISTSIZE:-1000} ; } ; } 2>/dev/null
         local PREFIX_LEN=${#PREFIX}
         declare -A HISTORY_HASH
+        local LINE_ORG=""
         local LINE=""
-        while read -u ${FC_FD[0]} LINE; do
+        local TAB=$'\t'
+        while IFS="" read -r -u ${FC_FD[0]} LINE_ORG; do
+            LINE="${LINE_ORG#$TAB}"
             if [[ -n "$LINE" && -z "${HISTORY_HASH[$LINE]}" && "${LINE:0:$PREFIX_LEN}" == "$PREFIX" ]]; then
                 HISTORY_HASH[$LINE]="1"
                 __bhslt_history_array+=( "$LINE" )
